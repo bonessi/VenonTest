a908fc917b47e50ed367b735c224d81c9e9adaf5
santiagobonessi <santiagobonessi@hotmail.com>
2019-04-03 21:57:32 -0300

    Commit Proyecto TestVenon

---
+++ CRUD-Estudiantes/CRUD-Estudiantes/Controllers/EnrollmentController.cs
@@ -0,0 +1,151 @@
+﻿using CRUD_Estudiantes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace CRUD_Estudiantes.Controllers
+{
+    public class EnrollmentController : Controller
+    {
+        // GET: Enrollment
+        public ActionResult Index()
+        {
+            try
+            {
+                using (var db = new VenonCollegeEntities())
+                {
+                    //List<Enrollment> lista = db.Enrollment.Where(e => e.Grade > 5).ToList();
+
+                    return View(db.Enrollment.ToList());
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Agregar(Enrollment e)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View();
+                using (var db = new VenonCollegeEntities())
+                {
+                    e.EnrollmentDate = DateTime.Now;
+
+                    db.Enrollment.Add(e);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Enrollment add error - " + ex.Message);
+                return View();
+            }
+
+        }
+
+        [HttpGet]
+        public ActionResult Editar(int id)
+        {
+            try
+            {
+                
+                using (var db = new VenonCollegeEntities())
+                {
+                    Enrollment en = db.Enrollment.Find(id);
+
+                    return View(en);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Enrollment edit error - " + ex.Message);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Editar(Enrollment e)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return View();
+                using (var db = new VenonCollegeEntities())
+                {
+                    Enrollment en = db.Enrollment.Find(e.EnrollmentID);
+                    en.Grade = e.Grade;
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+
+        public ActionResult Detalle(int id)
+        {
+            try
+            {
+                using (var db = new VenonCollegeEntities())
+                {
+                    Enrollment en = db.Enrollment.Find(id);
+
+                    return View(en);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public ActionResult Eliminar(int id)
+        {
+            try
+            {
+                using (var db = new VenonCollegeEntities())
+                {
+                    Enrollment en = db.Enrollment.Find(id);
+                    db.Enrollment.Remove(en);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+    }
+}
---
+++ CRUD-Estudiantes/CRUD-Estudiantes/Models/EnrollmentsCE.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_Estudiantes.Models
+{
+    public class EnrollmentsCE
+    {
+        public int EnrollmentID { get; set; }
+
+        [Display(Name = "Course")]
+        public int CourseID { get; set; }
+
+        [Display(Name = "Student")]
+        public int StudentID { get; set; }
+        
+        [Display(Name = "Enrollment Date")]
+        public System.DateTime EnrollmentDate { get; set; }
+        
+        public int Grade { get; set; }
+    }
+
+    [MetadataType(typeof(EnrollmentsCE))]
+    public partial class Enrollment
+    {
+        //public string CourseName { get; set; }
+        // public string StudentName { get; set; }
+
+        public string getCourseName(int p_courseID)
+        {
+            var db = new VenonCollegeEntities();
+
+            Course entCourse = db.Course.Find(p_courseID);
+
+            return entCourse.Title;
+        }
+
+        public string getStudentName(int p_personID)
+        {
+            var db = new VenonCollegeEntities();
+
+            Person entPerson  = db.Person.Find(p_personID);
+
+            string completeName = entPerson.LastName + " " + entPerson.FirstName;
+
+            return completeName;
+        }
+
+
+    }
+}
